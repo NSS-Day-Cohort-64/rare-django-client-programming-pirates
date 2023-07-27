2093e2c40d63c3c725556178ea0fd84389b7795b
Jonathan VanDuyne <jonathanvanduyne@gmail.com>
2023-07-27 15:56:41 -0400

    updates

--- src/components/nav/UserNavBar.js
+++ src/components/nav/UserNavBar.js
@@ -34,8 +34,8 @@ export const UserNavBar = ({ token, setToken, isAdmin, setIsAdmin }) => {
                         token
                             ?
                             <>
-                                <Link to="/" className="navbar-item">Posts</Link>;
-                                <Link to="/tagManager" className="navbar-item">Tag Manager</Link>;
+                                <Link to="/" className="navbar-item">Posts</Link>
+                                <Link to="/tagManager" className="navbar-item">Tag Manager</Link>
                             </>
                             :
                             ""
