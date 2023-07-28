3f1ab48d6f4ce5cbc9c1dc66a47257163b5549ce
Lance Buckley <erchancy@gmail.com>
2023-07-28 14:55:50 -0500

    Add profile page

--- src/APIManager.js
+++ src/APIManager.js
@@ -1,7 +1,7 @@
 export const getUser = async (id) => {
   const response = await fetch(`http://localhost:8088/users/${id}`);
-  const users = await response.json();
-  return users;
+  const user = await response.json();
+  return user;
 };
 
 export const getTheCategories = async () => {
