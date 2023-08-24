eaf3c508afecfca41796162af9d4538de38478bd
Sarah Saturday <sarahsaturday@gmail.com>
2023-08-24 16:32:00 -0500

    stashing

---
+++ src/components/posts/UserPosts/UserPostCommentView.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+
+export const UserPostCommentView = () => {
+    const navigate = useNavigate();
+    const { postId } = useParams();
+    const [commentContent, setCommentContent] = useState("");
+    const [postComments, setPostComments] = useState([]);
+    
+    const handleCommentSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`http://localhost:8000/comments`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Token ${localStorage.getItem("auth_token")}`,
+                },
+                body: JSON.stringify({
+                    post: postId,
+                    content: commentContent,
+                }),
+            });
+
+            if (response.ok) {
+                await response.json();
+                fetch(`http://localhost:8000/posts/${postId}`)
+                    .then(response => response.json())
+                    .then(postData => {
+                        setPostComments(postData.comments);
+                        navigate(`/posts/UserPosts/UserAllPosts/UserPostDetails/${postId}`); // Use navigate
+                    });
+
+                setCommentContent("");
+            } else {
+                console.error("Error adding comment");
+            }
+        } catch (error) {
+            console.error("Error adding comment:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetch(`http://localhost:8000/posts/${postId}`, {
+            headers: {
+                Authorization: `Token ${localStorage.getItem("auth_token")}`,
+            },
+        })
+            .then(response => response.json())
+            .then(postData => setPostComments(postData.comments)); // Assuming your API response structure has a "comments" field
+    }, []);
+
+
+    return (
+        <div>
+            <h1>Add Comment</h1>
+            <form onSubmit={handleCommentSubmit}>
+                <div>
+                    <label htmlFor="commentContent">Comment:</label>
+                    <textarea
+                        id="commentContent"
+                        value={commentContent}
+                        onChange={(e) => setCommentContent(e.target.value)}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <h2>Comments:</h2>
+                    <ul>
+                        {postComments && postComments.map(comment => (
+                            <li key={comment.id}>{comment.content}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <button type="submit">Add Comment</button>
+                <Link to={`/posts/UserPosts/UserAllPosts/UserPostDetails/${postId}`}>Back to Post</Link>
+            </form>
+        </div>
+    );
+}
--- src/components/posts/UserPosts/UserPostDetails.js
+++ src/components/posts/UserPosts/UserPostDetails.js
@@ -1,20 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
+import "./myPosts.css";
 
 export const UserSelectedPostDetails = () => {
     const { postId } = useParams();
     const [selectedPost, setSelectedPost] = useState([]);
+    const [postComments, setPostComments ] = useState([]);
 
     const getPosts = () => {
-        fetch(`http://localhost:8000/posts/${postId}`)
+        fetch(`http://localhost:8000/posts/${postId}`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${localStorage.getItem("auth_token")}`,
+            },
+        })
             .then(response => response.json())
             .then((postArray) => {
-                setSelectedPost([postArray])
+                setSelectedPost([postArray]);
             })
     }
 
+    const getComments = () => {
+        fetch(`http://localhost:8000/comments?post=${postId}`, {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Token ${localStorage.getItem("auth_token")}`,
+            },
+        })
+        .then(response => response.json())
+        .then((commentArray) => {
+            setPostComments(commentArray); 
+        })
+    }
+
     useEffect(() => {
-        getPosts()
+        getPosts();
+        getComments();
     }, []);
 
     return (
@@ -33,6 +54,17 @@ export const UserSelectedPostDetails = () => {
                     ))}
                 </article>
             </ul>
+            <div>
+                <h2>Comments:</h2>
+                <ul>
+                    {postComments.map(comment => (
+                        <li key={comment.id}>{comment.content} by {comment.author}</li>
+                    ))}
+                </ul>
+            </div>
+            <button>
+                <Link to={`/posts/${postId}/add-comment`}>Add Comment</Link>
+            </button>
         </div>
     );
 };
